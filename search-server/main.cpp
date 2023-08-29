84e9ca2ef33b01b5b6ba3dd73298155494caeadf
Alex Shishlo <kyockgb@yandex.ru>
2023-08-29 15:53:53 +0300

    ready exersice

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,15 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
-
+#include <iostream>
+using namespace std;
+const int BORDER_COUNT = 1000;
+int main() {
+    int counter=0;
+    
+    for (int i = 0; i < BORDER_COUNT; ++i)
+    {
+        if (i % 10 == 3 || (i % 100) / 10 == 3) { ++counter;}
+    }
+    cout << counter << endl;
+}
 // Закомитьте изменения и отправьте их в свой репозиторий.
