b86a933e6a691944d739fcc36ced8eaab45509ff
AlexNovoros87 <134471053+AlexNovoros87@users.noreply.github.com>
2023-10-22 21:02:07 +0300

    Updated request_queue method out class
    Updated request_queue method out class

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -8,20 +8,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
     // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
-        vector<Document> result = (*search_s).FindTopDocuments(raw_query, document_predicate);
-        if (result.empty()) ++empty_;
-
-        if (requests_.size() < min_in_day_) {
-            requests_.push_back({ IsEmptyRequest(result), raw_query });
-        }
-        else {
-            if (requests_.front().is_empty_request == true) --empty_;
-            requests_.pop_front();
-            requests_.push_back({ IsEmptyRequest(result), raw_query });
-        }
-        return result;
-    }
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate);
 
     vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status);
     vector<Document> AddFindRequest(const string& raw_query);
@@ -40,3 +27,19 @@ private:
     bool IsEmptyRequest(const vector<Document>& vctr) const;
 
 };
+
+template <typename DocumentPredicate>
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+    vector<Document> result = (*search_s).FindTopDocuments(raw_query, document_predicate);
+    if (result.empty()) ++empty_;
+
+    if (requests_.size() < min_in_day_) {
+        requests_.push_back({ IsEmptyRequest(result), raw_query });
+    }
+    else {
+        if (requests_.front().is_empty_request == true) --empty_;
+        requests_.pop_front();
+        requests_.push_back({ IsEmptyRequest(result), raw_query });
+    }
+    return result;
+}
